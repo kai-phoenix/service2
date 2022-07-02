b299071acdaa25f9d934a2c564aee062e6869ad0
kaito <zax04097@outlook.jp>
2022-07-02 17:47:15 +0900

    #15_2

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,8 @@
         <link rel='stylesheet' href="{{url(mix('css/app.css'))}}">
         <!-- Scripts -->
         <!-- <script src="{{ mix('js/app.js') }}" defer></script> -->
-        <script src="./js/app.js" defer></script>
+        <!-- <script src="./js/app.js" defer></script> -->
+        <script src="{{url(mix('js/app.js'))}}" defer></script>
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
